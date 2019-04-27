329795227293a8bd8929ef98527214b71bb314af
Muhammad Ihfazhillah <mihfazhillah@gmail.com>
2019-04-27 20:44:46 +0700

    add gina helper push function

--- .config/nvim/rplugin/python3/gina_helper.py
+++ .config/nvim/rplugin/python3/gina_helper.py
@@ -1,5 +1,6 @@
 """additional helper for gina nvim plugin"""
 import pynvim
+import subprocess
 
 
 @pynvim.plugin
@@ -11,6 +12,7 @@ class GinaHelper():
     @pynvim.command('ThisBranch', sync=True)
     def this_branch(self):
         """get this branch"""
+        self.nvim.call('gina#component#repo#branch')
         current_branch = self.nvim.command_output('echo gina#component#repo#branch()')
 
         branch_name_list = current_branch.split('/')
@@ -23,3 +25,20 @@ class GinaHelper():
 
         # change cursor into end of line
         self.nvim.command(f'call cursor(0, col("$"))')
+
+    @pynvim.function('GinaHelperPush', sync=False)
+    def ginahelperpush(self, _args):
+        """automate select the current branch"""
+        # 1. get current branch
+        self.nvim.call('gina#component#repo#branch')
+        current_branch = self.nvim.command_output('echo gina#component#repo#branch()')
+        # 2. get first remote
+        remotes = subprocess.getoutput('git remote').splitlines()
+        if len(remotes) > 1:
+            # TODO: do input
+            pass
+        else:
+            remote = remotes[0]
+        # 3. call push [remote] [branch] action
+        self.nvim.command(f'Gina push {remote} {current_branch}')
+
